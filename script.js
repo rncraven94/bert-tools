527da9c78f8e73e5fb3c2acaf676a9e90c050f6b
rncraven94 <neilcraven2013@gmail.com>
2023-08-27 22:09:32 -0400

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 use: "strict";
 
 console.log("hello");
+
+navigator.mediaDevices
+  .getUserMedia({ audio: true })
+  .then(function (stream) {
+    // You have access to the microphone stream (MediaStream object)
+  })
+  .catch(function (error) {
+    console.error("Error accessing microphone:", error);
+  });
+
+const audioContext = new AudioContext();
+const mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+const mediaRecorder = new MediaRecorder(stream);
+const recordedChunks = [];
+
+mediaRecorder.ondataavailable = function (event) {
+  if (event.data.size > 0) {
+    recordedChunks.push(event.data);
+  }
+};
+
+mediaRecorder.onstop = function () {
+  const audioBlob = new Blob(recordedChunks, { type: "audio/wav" });
+  // You can do something with the audioBlob, like playing, saving, or uploading it
+};
+
+// Start recording
+mediaRecorder.start();
+
+// Stop recording after a certain duration, or based on user action
+setTimeout(function () {
+  mediaRecorder.stop();
+}, recordingDuration);
+
+const recordedBuffer = await audioContext.decodeAudioData(
+  await audioBlob.arrayBuffer()
+);
+const playbackSource = audioContext.createBufferSource();
+playbackSource.buffer = recordedBuffer;
+playbackSource.connect(audioContext.destination);
+playbackSource.start();
